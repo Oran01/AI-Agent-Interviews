e199e7ffa7873d70ab6b19a04088da6f3599a1a6
Oran Alster <oranalster@Orans-MacBook-Air.local>
2025-04-27 18:17:38 +0300

    Fix: client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { initializeApp, getApp, getApps } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmXCxHrAAwtMfS_hWcMdwKsMlaWdmjb-U",
-  authDomain: "prepwise-96dd0.firebaseapp.com",
-  projectId: "prepwise-96dd0",
-  storageBucket: "prepwise-96dd0.firebasestorage.app",
-  messagingSenderId: "860435316841",
-  appId: "1:860435316841:web:d9d2264b1a50d9648ee2f8",
-  measurementId: "G-358M763KYK",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
